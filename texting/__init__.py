a590113165602a4fb63d3e28c063d11f83e8b8d4
hazen <hoyeungw@outlook.com>
2020-08-20 12:29:28 +0800

    add functions

--- texting/__init__.py
+++ texting/__init__.py
@@ -1,6 +1,7 @@
 # from .tap import link, tag, tags
 from .bracket import anglebr, br, brace, bracket, parenth, to_br
 from .chars import AEU, CO, COLF, COSP, CR, DA, DASH, DOSP, DOT, DT, ELLIP, LF, QT, RN, RT, RTSP, SP, TB, UL, VO
+from .fold import fold, fold_to_vector
 from .has_ansi import has_ansi, has_astral, has_han
 from .indexing import after_nontab, index_nontab
 from .lange import lange
@@ -8,3 +9,5 @@ from .lines import join_lines, liner
 from .pad import cpad, lpad, rpad
 from .ripper import ripper
 from .str_util import StrTemp
+from .str_value import str_value
+from .tap import link, tag, tags
